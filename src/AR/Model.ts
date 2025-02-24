4bdea2d684a1975d10ad88dd0754ce5dcb615d35
Frederick Roegiers <frederick@rogerthat.be>
2025-02-24 10:57:10 +0100

    wip

--- src/AR/Model.ts
+++ src/AR/Model.ts
@@ -5,11 +5,30 @@ export class Model {
     private loader: GLTFLoader;
     private model : THREE.Object3D | null = null;
 
-    constructor() {}
+    constructor(
+        private modelPath : string,
+        private scale : THREE.Vector3,
+        private position : THREE.Vector3 = new THREE.Vector3(0, 0, 0),
+        private rotation : THREE.Vector3 = new THREE.Vector3(0, 0, 0),
+        private hand : string  = "Right"
+    ) {
+        this.loader = new GLTFLoader();
+    }
 
-    public loadModel() {}
+    public loadModel(callback: (model: THREE.Object3D) => void) {
+        this.loader.load(this.modelPath, (gltf) => {
+            this.model = gltf.scene as THREE.Object3D;
+            this.model.scale.copy(this.scale);
+            this.model.position.copy(this.position);
+            this.model.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z);
+            callback(this.model);
+        });
+    }
 
-    public setPosition() {}
+    public setPosition(x: number, y: number, z: number) {
+        if(!this.model) return;
+        this.model.position.set(x, y, z);
+    }
 
     public setRotation() {}
 
