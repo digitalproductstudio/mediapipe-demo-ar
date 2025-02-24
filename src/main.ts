67bdbd3eebd417dabe436f466eefe68296a04b5e
Frederick Roegiers <frederick@rogerthat.be>
2025-02-24 19:39:24 +0100

    refactor: enhance code documentation and improve function structure

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ declare type RunningMode= "IMAGE" | "VIDEO";
 let runningMode : RunningMode = "VIDEO";
 let gestureRecognizer : GestureRecognizer | undefined;
 let isWebcamRunning : boolean = false;
-let lastVideoTime = -1;
 let results : GestureRecognizerResult | undefined = undefined;
 
 
@@ -32,7 +31,22 @@ const ARLayers = document.querySelector('#ar-layers') as HTMLElement;
 
 init();
 
-async function init() {
+/**
+ * Initializes the application by performing the following steps:
+ * 1. Checks if the browser supports the getUserMedia API.
+ * 2. Creates a gesture recognizer.
+ * 3. Enables the webcam.
+ * 4. Creates the scene for rendering.
+ * 5. Starts the webcam prediction loop.
+ * 
+ * Logs the progress of each step to the console.
+ * Catches and logs any errors that occur during initialization.
+ * 
+ * @async
+ * @function init
+ * @returns {Promise<void>} A promise that resolves when the initialization is complete.
+ */
+async function init() : Promise<void> {
   try {
     await hasGetUserMedia();
     console.log("User media available");
@@ -53,7 +67,20 @@ async function init() {
   }
 }
 
-async function createScene() {
+
+/**
+ * Asynchronously creates a 3D scene with predefined models and adds them to the scene.
+ * 
+ * This function initializes a new `Scene` object with the dimensions of the provided video element
+ * and a specified set of AR layers. It then creates two 3D models (`bottleabeer` and `TOKTOK`) with
+ * their respective file paths, scales, positions, and orientations. These models are subsequently
+ * added to the scene.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} A promise that resolves when the scene and models have been successfully created and added.
+ */
+async function createScene() : Promise<void> {
   SCENE = new Scene(video.videoWidth, video.videoHeight, ARLayers);
 
   let bottleabeer = new Model(
@@ -77,7 +104,19 @@ async function createScene() {
 
 }
 
-async function createGestureRecognizer() {
+/**
+ * Asynchronously creates a new gesture recognizer using the MediaPipe library.
+ * 
+ * This function loads the MediaPipe gesture recognizer web assembly file using the `FilesetResolver`
+ * and creates a new `GestureRecognizer` object with the specified options. The recognizer is configured
+ * to detect gestures from two hands and run in the specified mode. The recognizer is then stored in the
+ * `gestureRecognizer` variable.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} A promise that resolves when the gesture recognizer has been successfully created.
+ */
+async function createGestureRecognizer() : Promise<void> {
   // Load the MediaPipe gesture recognizer
   // It is a web assembly file, so we need to load it using the FilesetResolver
   const vision = await FilesetResolver.forVisionTasks(
@@ -97,7 +136,20 @@ async function createGestureRecognizer() {
   )
 
 }
-async function enableWebcam() {
+
+/**
+ * Asynchronously enables the webcam and starts the prediction loop.
+ * 
+ * This function toggles the `isWebcamRunning` flag to enable the webcam and starts the webcam video stream.
+ * It then waits for the video to load and sets the dimensions of the canvas element to match the video dimensions.
+ * The function then starts the webcam prediction loop using `requestAnimationFrame` to continuously predict gestures
+ * from the webcam video
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} A promise that resolves when the webcam is successfully enabled and the prediction loop has started.
+ */
+async function enableWebcam() : Promise<void> {
   isWebcamRunning = !isWebcamRunning;
   if(!video) return;
 
@@ -125,7 +177,20 @@ async function enableWebcam() {
     console.error(e);
   }
 }
-async function predictWebcam() {
+
+/**
+ * Asynchronously predicts gestures from the webcam video stream and renders the 3D models.
+ * 
+ * This function continuously predicts gestures from the webcam video stream using the gesture recognizer
+ * and updates the 3D models based on the detected landmarks. It clears the canvas, displays the landmarks,
+ * and renders the 3D models for each hand detected in the video stream. The function then requests the next
+ * animation frame to continue the prediction loop.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} A promise that resolves when the prediction loop has completed.
+ */
+async function predictWebcam() : Promise<void> {
   if(!video) return;
 
   // check when last predicted
@@ -144,30 +209,12 @@ async function predictWebcam() {
     // display landmarks
     displayLandmarks(canvasCtx, results);
     
-    // iterate over every landmark array
-    results?.landmarks.forEach((landmarks, index) => {
-
-      // get hand of this landmark array
-      const handedness = results?.handedness[index][0];
-      const hand = handedness?.displayName;
-
-      // check if we have a model that corresponds to this hand (e.g. no model needed for Left)
-      const model = SCENE.models.find((model) => model.getHand() === hand);
-
-      // position, rotate and scale the model to the hand
-      if(model) {
-        model.toggleVisibility(true);
-        map3DModel(landmarks, model);
-      }      
-    });
-
-    // hide the model when no hand is detected
-    toggleVisibility(results);
+    // render models, based on the landmarks
+    renderModels(results);
   }
 
   // by restoring the canvas, we can draw the results
   canvasCtx.restore();
-
 
   // rerun prediction, when all logic is done inside this method
   if(isWebcamRunning) {
@@ -176,6 +223,47 @@ async function predictWebcam() {
   }
 }
 
+/**
+ * Renders the 3D models based on the detected landmarks from the gesture recognizer results.
+ * 
+ * This function iterates over each set of landmarks and handedness in the gesture recognizer results and
+ * checks if a corresponding 3D model exists for the detected hand. If a model is found, the function toggles
+ * the model's visibility and maps the 3D model to the detected landmarks by setting the position, rotation,
+ * and scale of the model. If no hand is detected, the function hides the model.
+ * 
+ * @function
+ * @param {GestureRecognizerResult} results - The gesture recognizer results containing the detected landmarks and handedness.
+ */
+function renderModels(results: GestureRecognizerResult) {
+  // iterate over every landmark array
+  results.landmarks.forEach((landmarks, index) => {
+    // get hand of this landmark array
+    const handedness = results.handedness[index][0];
+    const hand = handedness.displayName;
+
+    // check if we have a model that corresponds to this hand (e.g. no model needed for Left)
+    const model = SCENE.models.find((model) => model.getHand() === hand);
+
+    // position, rotate and scale the model to the hand
+    if(model) {
+      model.toggleVisibility(true);
+      map3DModel(landmarks, model);
+    }      
+  });
+
+  // hide the model when no hand is detected
+  toggleVisibility(results);
+}
+
+/**
+ * Toggles the visibility of the 3D models based on the detected hands in the gesture recognizer results.
+ * 
+ * This function iterates over each 3D model in the scene and checks if the model corresponds to a detected hand
+ * in the gesture recognizer results. If the model does not correspond to a detected hand, the function hides the model.
+ * 
+ * @function
+ * @param {GestureRecognizerResult | undefined} results - The gesture recognizer results containing the detected hands.
+ */
 function toggleVisibility(results : GestureRecognizerResult | undefined) {
   SCENE.models.forEach((model) => {
     const handIndex = results?.handedness.findIndex(
@@ -187,6 +275,18 @@ function toggleVisibility(results : GestureRecognizerResult | undefined) {
   });
 }
 
+/**
+ * Maps the 3D model to the detected landmarks by setting the position, rotation, and scale of the model.
+ * 
+ * This function maps the 3D model to the detected landmarks by normalizing the x, y, and z values of the landmarks
+ * and calculating the position, rotation, and scale of the model based on the detected hand. The function sets the
+ * position of the model based on the palm base landmark, the rotation based on the angle between the index finger and
+ * the thumb, and the scale based on the distance between the index finger and the thumb.
+ * 
+ * @function
+ * @param {Array<{ x: number, y: number, z: number }>} landmarks - The detected landmarks for the hand.
+ * @param {Model} model - The 3D model to map to the detected landmarks.
+ */
 async function map3DModel(
   landmarks : { x: number, y: number, z: number }[],
   model : Model
@@ -196,26 +296,23 @@ async function map3DModel(
   const thumb = landmarks[4];
 
   // position
+  // by normalizing the x, y and z values, we can map them to the 3D space
   let mX = (palmBase.x - 0.5) * 2;
   let mY = -(palmBase.y - 0.5) * 2;
   let mZ = -palmBase.z * 2;
   model.setPosition(mX, mY, mZ);
 
-  // rotate
+  // rotation calculation
+  // calculate the angle between the index finger and the thumb
   const midX = (indexFinger.x + thumb.x) / 2;
   const midY = (indexFinger.y + thumb.y) / 2;
-
   const dX = midX - palmBase.x;
   const dY = midY - palmBase.y;
-
   const angle = -Math.atan2(dY, dX) - Math.PI / 2;
-
+  // set rotation
   model.setRotation(0, 0, angle);
-
 
   // scale
   const scale = Math.sqrt(dX * dX + dY * dY) * 0.05;
   model.setScale(scale, scale, scale);
-
-  
 }
