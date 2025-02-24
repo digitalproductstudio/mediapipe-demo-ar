d69c048e15b0af8e3afa8b3fd0851d754c33dabd
Frederick Roegiers <frederick@rogerthat.be>
2025-02-24 09:19:47 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,20 @@ import {
   GestureRecognizerResult
 } from "@mediapipe/tasks-vision";
 
+
+// declare variables
+declare type RunningMode= "IMAGE" | "VIDEO";
+let runningMode : RunningMode = "VIDEO";
+let gestureRecognizer : GestureRecognizer | undefined;
+let isWebcamRunning : boolean = false;
+let lastVideoTime = -1;
+let results : GestureRecognizerResult | undefined = null;
+
+// declare DOM elements
+const video = document.querySelector('#webcam') as HTMLVideoElement;
+const canvasElement = document.querySelector('#output_canvas') as HTMLCanvasElement;
+const canvasCtx = canvasElement.getContext('2d') as CanvasRenderingContext2D;
+const gestureOutput = document.querySelector('#gesture_output') as HTMLDivElement;
+const btnEnableWebcam = document.querySelector('#webcamButton') as HTMLButtonElement;
+const ARLayers = document.querySelector('#ar-layers') as HTMLElement;
+
